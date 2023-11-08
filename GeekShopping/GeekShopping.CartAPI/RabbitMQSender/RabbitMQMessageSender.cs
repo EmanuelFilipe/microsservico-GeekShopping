ab6a3034487aef3bba91c32200ee39ec81327dd5
efilipe <efilipe@magnasistemas.com.br>
2023-11-08 07:09:51 -0300

    ajuste de conexão do sender

--- GeekShopping/GeekShopping.CartAPI/RabbitMQSender/RabbitMQMessageSender.cs
+++ GeekShopping/GeekShopping.CartAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -4,6 +4,7 @@ using RabbitMQ.Client;
 using System.Text;
 using System.Text.Json;
 using GeekShopping.CartAPI.Messages;
+using System;
 
 namespace GeekShopping.CartAPI.RabbitMQSender
 {
@@ -23,20 +24,14 @@ namespace GeekShopping.CartAPI.RabbitMQSender
 
         public void SendMessage(BaseMessage message, string queueName)
         {
-            var factory = new ConnectionFactory
+            if (ConnectionExists())
             {
-                HostName = _hostName,
-                Password = _password,
-                UserName = _userName,
-            };
+                using var channel = _connection.CreateModel();
+                channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+                byte[] body = GetMessageAsByteArray(message);
 
-            _connection = factory.CreateConnection();
-
-            using var channel = _connection.CreateModel();
-            channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
-            byte[] body = GetMessageAsByteArray(message);
-
-            channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+            }
         }
 
         private byte[] GetMessageAsByteArray(BaseMessage message)
@@ -51,6 +46,34 @@ namespace GeekShopping.CartAPI.RabbitMQSender
             var body = Encoding.UTF8.GetBytes(json);
 
             return body;
+        }
+
+        private bool ConnectionExists()
+        {
+            if (_connection != null) return true;
+
+            CreateConnection();
+
+            return _connection != null;
+        }
+
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = _hostName,
+                    Password = _password,
+                    UserName = _userName,
+                };
+
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
